9e63357339c30270757de5c7e0617028fddb8b58
sebastianAcunaZionit <sacuna@zionit.cl>
2024-01-26 09:54:52 -0300

    add: base created

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:widget_gallery_sa/router/app_router.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerConfig: appRouter,
+    );
+  }
+}
